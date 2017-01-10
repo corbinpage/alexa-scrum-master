0b3fcf5eaccf566ef631c97340d6eb588fafd0a5
Corbin Page <corbin.page@gmail.com>
2017-01-10 18:41:10 -0500

    Altered and resubmitted for Amazon certification

--- src/index.js
+++ src/index.js
@@ -1,74 +1,9 @@
 /**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.amazon.com/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-    */
-
-/**
- * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
- *
- * - Session State: Handles a multi-turn dialog model.
- * - Custom slot type: demonstrates using custom slot types to handle a finite set of known values
- * - SSML: Using SSML tags to control how Alexa renders the text-to-speech.
- *
- * Examples:
- * Dialog model:
- *  User: "Alexa, ask Wise Guy to tell me a knock knock joke."
- *  Alexa: "Knock knock"
- *  User: "Who's there?"
- *  Alexa: "<phrase>"
- *  User: "<phrase> who"
- *  Alexa: "<Punchline>"
- */
-
-/**
  * App ID for the skill
  */
-var APP_ID = 'amzn1.ask.skill.da37cf73-e5c1-4cc5-88b5-90d8198287dd'; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+var APP_ID = 'amzn1.ask.skill.da37cf73-e5c1-4cc5-88b5-90d8198287dd';
 
-/**
- * Array containing knock knock jokes.
- */
 var cardTitle = 'Scrum Master';
-
-/**
- * Array containing knock knock jokes.
- */
-var JOKE_LIST = [{
-    setup: "To",
-    speechPunchline: "Correct grammar is <break time=\"0.2s\" /> to whom.",
-    cardPunchline: "Correct grammar is 'to whom'."
-  },
-  { setup: "Beets!", speechPunchline: "Beats me!", cardPunchline: "Beats me!" }, {
-    setup: "Little Old Lady",
-    speechPunchline: "I didn't know you could yodel!",
-    cardPunchline: "I didn't know you could yodel!"
-  }, {
-    setup: "A broken pencil",
-    speechPunchline: "Never mind. It's pointless.",
-    cardPunchline: "Never mind. It's pointless."
-  },
-  { setup: "Snow", speechPunchline: "Snow use. I forgot", cardPunchline: "Snow use. I forgot" }, {
-    setup: "Boo",
-    speechPunchline: "Aww <break time=\"0.3s\" /> it's okay <break time=\"0.3s\" /> don't cry.",
-    cardPunchline: "Aww, it's okay, don't cry."
-  }, {
-    setup: "Woo",
-    speechPunchline: "Don't get so excited, it's just a joke",
-    cardPunchline: "Don't get so excited, it's just a joke"
-  }, {
-    setup: "Spell",
-    speechPunchline: "<say-as interpret-as=\"characters\">who</say-as>",
-    cardPunchline: "w.h.o"
-  },
-  { setup: "Atch", speechPunchline: "I didn't know you had a cold!", cardPunchline: "I didn't know you had a cold!" },
-  { setup: "Owls", speechPunchline: "Yes, they do.", cardPunchline: "Yes, they do." },
-  { setup: "Berry!", speechPunchline: "Berry nice to meet you.", cardPunchline: "Berry nice to meet you." }
-];
 
 /**
  * The AlexaSkill prototype and helper functions
@@ -78,8 +13,6 @@ var AlexaSkill = require('./AlexaSkill');
 /**
  * ScrumMaster is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
 var ScrumMaster = function() {
   AlexaSkill.call(this, APP_ID);
@@ -104,7 +37,7 @@ ScrumMaster.prototype.eventHandlers.onSessionStarted = function(sessionStartedRe
 ScrumMaster.prototype.eventHandlers.onLaunch = function(launchRequest, session, response) {
   console.log("ScrumMaster onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
 
-  handleTellMeAJokeIntent(session, response);
+  handleStartScrumIntent(session, response);
 };
 
 /**
@@ -126,8 +59,12 @@ ScrumMaster.prototype.intentHandlers = {
     handleEndScrumIntent(session, response);
   },
 
+  "TellScrumTimeIntent": function(intent, session, response) {
+    handleTellScrumTimeIntent(session, response);
+  },
+
   "AMAZON.HelpIntent": function(intent, session, response) {
-    var speechText = "You can say, start scrum or end scrum.";
+    var speechText = "You can ask Scrum Master to start the scrum or end the scrum.";
 
     var speechOutput = {
       speech: speechText,
@@ -176,6 +113,27 @@ function handleStartScrumIntent(session, response) {
   };
 
   response.tellAndWait(speechOutput);
+}
+
+function handleTellScrumTimeIntent(session, response) {
+  var speechText = "";
+
+  if (session.attributes.startTime) {
+    var diffMs = (Date.now() - session.attributes.startTime);
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+    var diffMinsText = diffMins < 1 ? "less than one minute" : diffMins < 2 ? "1 minute" : diffMins + " minutes";
+
+    speechText = "Your scrum has been going for " + diffMinsText + " so far.";
+  } else {
+    speechText = "You don't currently have an active Scrum.";
+  }
+
+  var speechOutput = {
+    speech: speechText,
+    type: AlexaSkill.speechOutputType.PLAIN_TEXT
+  };
+
+  response.tell(speechOutput);
 }
 
 function handleEndScrumIntent(session, response) {
